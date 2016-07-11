56a7129f40faf22c62c3c103a467db1ec1d28b5b
rodolphe.lepigre <rodolphe.lepigre@univ-savoie.fr>
2016-07-11 12:18:56 +0000

    Clening of submljs.ml + ported to new IO (except the file stuff)

--- submljs.ml
+++ submljs.ml
@@ -3,51 +3,55 @@ open Parser
 open Io
 
 let js_object = Js.Unsafe.variable "Object"
-let postMessage = Js.Unsafe.variable "postMessage"
-let syncloadsubmlfile = Js.Unsafe.variable "syncloadsubmlfile"
+let js_self   = Js.Unsafe.variable "self"
+let post_msg  = Js.Unsafe.variable "postMessage"
+let syncload  = Js.Unsafe.variable "syncloadsubmlfile"
 
 let onmessage event =
-  let filename = Js.to_string event##data##fname in
-  let s = Js.to_string event##data##args in
-  let b = handle_exception (full_of_string filename) s in
+  let fname = Js.to_string event##data##fname in
+  let args = Js.to_string event##data##args in
+  let res = handle_exception (full_of_string fname) args in
   Io.log "Editor content loaded\n%!";
-  let result = if b then Js.string "OK" else Js.string "ERROR" in
+  let result = Js.string (if res then "OK" else "ERROR") in
   let response = jsnew js_object () in
-  Js.Unsafe.set response (Js.string "typ") (Js.string "result");
-  Js.Unsafe.set response (Js.string "fname") filename;
+  Js.Unsafe.set response (Js.string "typ")    (Js.string "result");
+  Js.Unsafe.set response (Js.string "fname")  fname;
   Js.Unsafe.set response (Js.string "result") result;
-  Js.Unsafe.call postMessage (Js.Unsafe.variable "self") [|Js.Unsafe.inject response|]
+  let arg = Js.Unsafe.inject response in
+  Js.Unsafe.call post_msg js_self [|arg|]
 
-let _ = Js.Unsafe.set (Js.Unsafe.variable "self") (Js.string "onmessage") onmessage
-
-let buf = Buffer.create 256
-let fbuf = formatter_of_buffer buf
-
-let output = fun chname format ->
-  Buffer.clear buf;
-  kfprintf (fun _fbuf ->
-    let s = Js.string (Buffer.contents buf) in
+let output : string -> formatter = fun chname ->
+  let buf = Buffer.create 256 in
+  let out = Buffer.add_substring buf in
+  let flush () =
+    let s = Buffer.contents buf in
+    Buffer.clear buf;
     let response = jsnew js_object () in
     Js.Unsafe.set response (Js.string "typ") (Js.string chname);
-    Js.Unsafe.set response (Js.string "result") s;
-    ignore (Js.Unsafe.call postMessage (Js.Unsafe.variable "self") [|Js.Unsafe.inject response|]);
-    Buffer.clear buf) fbuf format
+    Js.Unsafe.set response (Js.string "result") (Js.string s);
+    ignore (Js.Unsafe.call post_msg js_self [|Js.Unsafe.inject response|])
+  in
+  make_formatter out flush
 
-(* FIXME
-let _ =
-  let open Io in
-  fmts.out <- (fun fmt -> output "stdout" fmt);
-  fmts.log <- (fun fmt -> output "log"    fmt);
-  fmts.err <- (fun fmt -> output "stderr" fmt)
 
+(*
 let _ = io.files  <- (fun filename  ->
   let args = [|Js.Unsafe.inject (Js.string filename)|] in
-  let res = Js.Unsafe.fun_call  syncloadsubmlfile args in
+  let res = Js.Unsafe.fun_call  syncload args in
   let s = Js.to_string res in
   Input.buffer_from_string ~filename s)
 *)
 
+
 let _ =
-  let s = Io.file "lib/prelude.typ" in
-  ignore (handle_exception full_of_buffer s);
+  (* Setup the IO stuff. *)
+  fmts.out <- output "stdout";
+  fmts.err <- output "stderr";
+  fmts.log <- output "log";
+  fmts.tex <- output "tex";
+  (* FIXME file stuff *)
+  (* Register callback. *)
+  Js.Unsafe.set js_self (Js.string "onmessage") onmessage;
+  (* Load the prelude. *)
+  ignore (handle_exception full_of_buffer (Io.file "lib/prelude.typ"));
   Io.log "File \"lib/prelude.typ\" loaded\n%!"
