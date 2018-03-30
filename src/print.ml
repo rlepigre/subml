308d4465b2b4cb5ace76b655b72f1f2ccb150caa
Rodolphe Lepigre <rodolphe.lepigre@inria.fr>
2018-03-30 18:04:01 +0200

    Fixed compilation on 4.03

--- src/print.ml
+++ src/print.ml
@@ -8,6 +8,22 @@ open Pos
 open Compare
 open LibTools
 
+module String = struct
+  include String
+
+  (* Borrowed from recent version of OCaml. *)
+  let split_on_char sep s =
+    let r = ref [] in
+    let j = ref (length s) in
+    for i = length s - 1 downto 0 do
+      if unsafe_get s i = sep then begin
+        r := sub s (i + 1) (!j - i - 1) :: !r;
+        j := i
+      end
+    done;
+    sub s 0 !j :: !r
+end
+
 (** control some differences when printing for LaTeX or GraphML*)
 type print_mode = TeX | Gml | Asc
 
